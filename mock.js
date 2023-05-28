39c0e5d86c4094c5a2599b943d64587806f22ca7
abdulazizv <abdulazizheir@gmail.com>
2023-05-28 10:27:16 +0500

    project started

--- mock.js
+++ mock.js
@@ -4,3 +4,16 @@ const PORT = 3000;
 app.use(() => {})
 app.get("/",(req,res) => {})
 app.post("/",(req,res) => {})
+app.put("/",(req,res) => {})
+app.delete("/",(req,res) => {})
+app.listen(PORT,() => {});
+/*
+req.body
+req.params
+req.query
+req.headers
+ */
+res.send("")
+res.json([])
+res.status(200).json({});
+res.sendFile(path)
