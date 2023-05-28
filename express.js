c36de79e49c28be05e7cabbc304ee0efd9f5606f
abdulazizv <abdulazizheir@gmail.com>
2023-05-28 10:43:34 +0500

    added listen and status and send

--- express.js
+++ express.js
@@ -1,1 +1,66 @@
-const http = require("http")
+const http = require('http');
+
+function express() {
+    const app = {
+        routes: [],
+        use: function (middleware) {
+            this.routes.push({ method: 'USE', middleware });
+        },
+        get: function (path, handler) {
+            this.routes.push({ method: 'GET', path, handler });
+        },
+        post: function (path, handler) {
+            this.routes.push({ method: 'POST', path, handler });
+        },
+        put: function (path, handler) {
+            this.routes.push({ method: 'PUT', path, handler });
+        },
+        delete: function (path, handler) {
+            this.routes.push({ method: 'DELETE', path, handler });
+        },
+        listen: function (port, callback) {
+            const server = http.createServer((req, res) => {
+                const matchingRoutes = this.routes.filter(
+                    (route) =>
+                        (route.method === 'USE' || route.method === req.method) &&
+                        (route.path === req.url || route.path === '*')
+                );
+
+                let i = 0;
+                function next() {
+                    const route = matchingRoutes[i++];
+                    if (route) {
+                        if (route.method === 'USE') {
+                            route.middleware(req, res, next);
+                        } else {
+                            let statusCode = 200;
+                            let responseBody = '';
+
+                            res.status = (status) => {
+                                statusCode = status;
+                                return res;
+                            };
+
+                            res.send = (body) => {
+                                responseBody = body;
+                                res.statusCode = statusCode;
+                                res.setHeader('Content-Type', 'text/plain');
+                                res.end(responseBody);
+                            };
+
+                            route.handler(req, res);
+                        }
+                    }
+                }
+
+                next();
+            });
+
+            server.listen(port, callback);
+        },
+    };
+
+    return app;
+}
+
+module.exports = express;
--- mock.js
+++ mock.js
@@ -1,19 +1,24 @@
 const express = require("./express");
 const app = express();
 const PORT = 3000;
-app.use(() => {})
-app.get("/",(req,res) => {})
-app.post("/",(req,res) => {})
-app.put("/",(req,res) => {})
-app.delete("/",(req,res) => {})
-app.listen(PORT,() => {});
+// app.use(() => {})
+app.get("/",(req,res) => {
+    return res.status(200).send("Hello world");
+
+})
+// app.post("/",(req,res) => {})
+// app.put("/",(req,res) => {})
+// app.delete("/",(req,res) => {})
+app.listen(PORT,() => {
+    console.log("Server running at 3000 port")
+});
 /*
 req.body
 req.params
 req.query
 req.headers
  */
-res.send("")
-res.json([])
-res.status(200).json({});
-res.sendFile(path)
+// res.send("")
+// res.json([])
+// res.status(200).json({});
+// res.sendFile(path)
