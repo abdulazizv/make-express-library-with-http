a4bced13a3fee5b376b4c378dcf48d70e7ac4ab7
abdulazizv <abdulazizheir@gmail.com>
2023-05-28 10:50:34 +0500

    started

--- mock.js
+++ mock.js
@@ -1,12 +1,26 @@
 const express = require("./express");
 const app = express();
 const PORT = 3000;
+
+const getData = async (url) => {
+    const response = await fetch(url)
+    const json = await response.json()
+
+    return json;
+}
+
 // app.use(() => {})
 app.get("/",(req,res) => {
     return res.status(200).send("Hello world");
-
 })
-// app.post("/",(req,res) => {})
+app.post("/",async (req,res) => {
+        const data = await getData("https://jsonplaceholder.typicode.com/users/1");
+        const resp = {
+            status:"OK",
+            user:data
+        }
+        res.send(JSON.stringify(data))
+})
 // app.put("/",(req,res) => {})
 // app.delete("/",(req,res) => {})
 app.listen(PORT,() => {
